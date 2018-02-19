f97024faacfc2aa214bf3f7c696e75ccb83cbb11
Kévin ISTRIA <istria.kevin@gmail.com>
2018-02-19 16:43:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kevin/retrofire/RetroFire.java
@@ -0,0 +1,13 @@
+package com.example.kevin.retrofire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RetroFire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retro_fire);
+    }
+}
