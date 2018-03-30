59c1e62f0cd2b406358567332c13fcf8cfb7e1eb
Kévin ISTRIA <istria.kevin@gmail.com>
2018-03-30 15:32:45 +0200

    correction bug apparition vaisseaux

--- app/src/main/java/com/example/kevin/retrofire/GameLoopThread.java
+++ app/src/main/java/com/example/kevin/retrofire/GameLoopThread.java
@@ -25,6 +25,11 @@ public class GameLoopThread extends Thread {
 
     @Override
     public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         while (isRunning) {
             try {
                 canvas = surfaceHolder.lockCanvas();
